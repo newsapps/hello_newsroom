dad2f0200140485211f0acb7afce606d01bb67ba
Joe Germuska <joe@germuska.com>
2010-02-17 18:18:54 -0600

    some more cleanup

--- hello_newsroom/configs/production/settings.py
+++ hello_newsroom/configs/production/settings.py
@@ -4,26 +4,19 @@ from hello_newsroom.configs.common.settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-# Database
-DATABASE_HOST = 'db.tribapps.com'
-DATABASE_PORT = '5433'
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://media.tribapps.com/hello_newsroom/'
+MEDIA_URL = 'http://your-bucket-name.s3.amazonaws.com/hello_newsroom/'
 
 # Predefined domain
-MY_SITE_DOMAIN = 'hello_newsroom.tribapps.com'
+MY_SITE_DOMAIN = 'your-ec2-instance-dns-name.amazonaws.com'
 
 # Email
-EMAIL_HOST = 'mail.tribapps.com'
+EMAIL_HOST = 'mail'
 
 # Caching
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
-
-# S3
-AWS_S3_URL = 's3://media.tribapps.com/hello_newsroom/'
 
 # logging
 import logging.config
--- hello_newsroom/configs/staging/settings.py
+++ hello_newsroom/configs/staging/settings.py
@@ -3,17 +3,13 @@ from hello_newsroom.configs.common.settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# Database
-DATABASE_HOST = 'localhost'
-DATABASE_PORT = '5433'
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://media-beta.tribapps.com/hello_newsroom/'
+MEDIA_URL = 'http://your-bucket-name.s3.amazonaws.com/hello_newsroom/'
 
 # Predefined domain
-MY_SITE_DOMAIN = 'ec2-204-236-246-222.compute-1.amazonaws.com'
+MY_SITE_DOMAIN = 'your-ec2-instance-dns-name.amazonaws.com'
 
 # Email
 EMAIL_HOST = 'localhost'
@@ -21,13 +17,10 @@ EMAIL_HOST = 'localhost'
 # Caching
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 
-# S3
-AWS_S3_URL = 's3://media-beta.tribapps.com/hello_newsroom/'
-
 # GOOGLE_MAPS_API_KEY = 'ABQIAAAA3uGjGrzq3HsSSbZWegPbIhSMhkig1Gd5B_2j4H1Xz7hsATFBFhSnBeYqZ7F7xlyJh-_KEClsWgAO6Q' # all amazonaws.com
 
-# Trib IPs for security
-INTERNAL_IPS = ('163.192.12.84','163.192.12.108','163.192.12.32')
+# If you want to use Django Debug Toolbar, you need to list your IP address here
+INTERNAL_IPS = ('0.0.0.0')
 
 # logging
 import logging.config
